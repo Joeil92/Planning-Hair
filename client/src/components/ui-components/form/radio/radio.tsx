786545a9a088e9fe406ceb3d778c3c22e477d726
Arkansiel92 <yaj92250@hotmail.fr>
2024-01-31 16:54:44 +0100

    front: remove dark theme

--- client/src/components/ui-components/form/radio/radio.tsx
+++ client/src/components/ui-components/form/radio/radio.tsx
@@ -13,19 +13,19 @@ interface Props {
 export default function Radio({ name, value, setValue, label, required = false, control }: Props) {
     return (
         <Controller
-            render={(fields) => (
+            render={({ field }) => (
                 <Container>
                     <input 
-                        {...fields}
+                        {...field}
                         type="radio"
                         value={value}
                         onChange={() => setValue(name, value)}
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                        checked={fields.field.value === value}
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                        checked={field.value === value}
                     />
                     <label 
                         htmlFor={name} 
-                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="ms-2 text-sm font-medium text-gray-900"
                     >{label}</label>
                 </Container>
             )}
--- client/src/components/ui-components/form/submit/submit.tsx
+++ client/src/components/ui-components/form/submit/submit.tsx
@@ -6,7 +6,7 @@ export default function Submit({ value }: Props) {
     return (
         <input 
             type="submit" 
-            className="my-3 text-white bg-zinc-800 hover:bg-zinc-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="my-3 text-white bg-zinc-800 hover:bg-zinc-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
             value={value} 
         />
     )

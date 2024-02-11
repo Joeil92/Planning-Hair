d150826bcb239a83a2d17eac4ec6b7e768c5da75
clement murça <clem.murca@gmail.com>
2024-02-11 19:31:42 +0100

    .

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Container from "@PH/components/ui-components/container/container";
 import Typography from "@PH/components/ui-components/typography/typography";
 import styles from "./page.module.css";
@@ -7,27 +7,37 @@ import { useState } from "react";
 import CompanyForm from "@PH/forms/companyForm/companyForm";
 
 export default function Page() {
-    const [formStep, setFormStep] = useState<"register" | "company">("register");
+  const [formStep, setFormStep] = useState<"register" | "company">("register");
 
-    return (
-        <Container className="h-screen flex">
-            <Container className="w-6/12 h-screen inline-block">
-                <Container className="w-96 h-80 my-32 m-auto">
-                    {formStep === "register"
-                        ? <>
-                            <Typography tag="h1" className="text-center mb-16">Super, un nouveau membre !</Typography>
-                            <RegistrationForm handleForm={setFormStep} />
-                        </>
-                        : <>
-                            <Typography tag="h1" className="text-center">Présentez-nous votre entreprise !</Typography>
-                            <Typography className="mb-16 text-center">Les informations pourront être changé ultérieurement</Typography>
-                            <CompanyForm />
-                        </>}
-                </Container>
-            </Container>
-            <Container className={`w-6/12 h-screen inline-block ${styles.rightContainer}`}>
-                <></>
-            </Container>
+  return (
+    <Container className="h-screen flex">
+      <Container className="w-6/12 h-screen inline-block">
+        <Container className="w-96 h-80 my-32 m-auto">
+          {formStep === "register" ? (
+            <>
+              <Typography tag="h1" className="text-center mb-16">
+                Super, un nouveau membre !
+              </Typography>
+              <RegistrationForm handleForm={setFormStep} />
+            </>
+          ) : (
+            <>
+              <Typography tag="h1" className="text-center">
+                Présentez-nous votre entreprise !
+              </Typography>
+              <Typography className="mb-16 text-center">
+                Les informations pourront être changé ultérieurement
+              </Typography>
+              <CompanyForm />
+            </>
+          )}
         </Container>
-    )
+      </Container>
+      <Container
+        className={`w-6/12 h-screen inline-block ${styles.rightContainer}`}
+      >
+        <></>
+      </Container>
+    </Container>
+  );
 }
